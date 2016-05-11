1ecc0aff22dd0517d3fee9f1e0e3190433f8155f
Suui <suui.gd@gmail.com>
2016-05-11 03:48:13 +0200

    Added TODO list

--- KataMarsRoverTest/MarsRoverShould.cs
+++ KataMarsRoverTest/MarsRoverShould.cs
@@ -2,7 +2,10 @@
 using NUnit.Framework;
 
 /* TODO
-	
+	- Start in the middle of Mars facing north
+	- Face North, East, South or West
+	- Be able to rotate to the left or to the right in single steps
+	- Be able to move forward in single steps
 */
 
 namespace KataMarsRoverTest
