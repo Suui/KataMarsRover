1e3a471d4f637308f944d4e84fbaabdd214588ef
Suui <suui.gd@gmail.com>
2016-05-11 03:45:10 +0200

    Starting point

---
+++ KataMarsRoverTest/MarsRoverShould.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+/* TODO
+	
+*/
+
+namespace KataMarsRoverTest
+{
+	[TestFixture]
+	class MarsRoverShould
+	{
+		[Test]
+		public void test_method()
+		{
+			
+		}
+	}
+}
